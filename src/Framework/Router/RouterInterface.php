2152ac8576ef269fc63c8a7a21063ca856ab4484
alecgunnar <alecgunnar@gmail.com>
2016-09-13 09:20:35 -0400

    Added router interface

---
+++ src/Framework/Router/RouterInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Framework\Router;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Framework\Router\Route\RouteInterface;
+
+interface RouterInterface
+{
+    /**
+     * @var int
+     */
+    const STATUS_FOUND = 200;
+
+    /**
+     * @var int
+     */
+    const STATUS_NOT_FOUND = 404;
+
+    /**
+     * @var int
+     */
+    const STATUS_NOT_ALLOWED = 405;
+
+    /**
+     * Process the given request and return the status
+     * of the routing attempt
+     *
+     * @param ServerRequestInterface $request
+     * @return int
+     */
+    public function processRequest(ServerRequestInterface $request): int;
+
+    /**
+     * Get the most recently matched route
+     *
+     * @return RouteInterface
+     */
+    public function getRoute(): RouteInterface;
+}

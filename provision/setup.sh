2470c623a26f9671e836777c77a26e9aed5fbfb7
alecgunnar <alecgunnar@gmail.com>
2016-09-15 19:16:46 -0400

    Worked on provision script

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y nginx nodejs npm mysql-server php-fpm php php-zip php-mysql
+apt-get install -y nginx nodejs npm php-fpm php php-zip php-mysql
 
 cat provision/host.nginx > /etc/nginx/sites-available/default
 

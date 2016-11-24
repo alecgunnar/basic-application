ef4b4a6a7ed24bd9588c4682bd72c339f1567599
alecgunnar <alecgunnar@gmail.com>
2016-11-24 11:01:56 -0500

    Loading config from framework

--- test/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Get the composer autoloader
-require(dirname(__DIR__) . '/vendor/autoload.php');
-
-// Define the fixture path to make the directory
-// easier to access.
-define('FIXTURE_PATH', __DIR__ . '/fixture');

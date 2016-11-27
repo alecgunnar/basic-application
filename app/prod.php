2d9912b8f3080a363817b99582f1b440eeb01aa7
alecgunnar <alecgunnar@gmail.com>
2016-11-27 11:39:34 -0500

    Updated default environment scripts

--- app/prod.php
+++ app/prod.php
@@ -1,10 +1,12 @@
 <?php
 
+use GuzzleHttp\Psr7\ServerRequest;
+
 $root = dirname(__DIR__);
 require_once($root . '/vendor/autoload.php');
 
 $container = Maverick\bootstrap($root);
 $app = $container->get('application');
 
-$response = $app->handleRequest();
+$response = $app->handleRequest(ServerRequest::fromGlobals());
 $app->sendResponse($response);
